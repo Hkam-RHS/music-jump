5ecd7d2c13975d1bd1ec2a0375ae6691a4c43e73
Christie Reindle <creindle@hawaii.edu>
2017-03-18 06:17:19 -1000

    finished game

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var spaceKey;
 var ground;
 var player;
 var obstacle;
+var obstacle2;
 
 var GAME_WIDTH = 800;
 var GAME_HEIGHT = 600;
@@ -16,7 +17,7 @@ function preload(){
 
     //These four things sets the assets for the game. If you want to add music or images, there is where you would preload it.
     game.load.image('background', 'assets/background.png');
-    game.load.image('player', 'assets/player.png');
+    game.load.image('player', 'assets/prehistoric_isle_3_in_1942_player_1_plane_by_theprinceofmars-d8nc9i1 copy.png');
     game.load.image('ground', 'assets/wallHorizontal.png');
     game.load.image('obstacle', 'assets/wallVertical.png');
     //If you'd like to load music files, the format would look like this. game.load.audio('[name of music]', ['[location for music file]']);
@@ -58,12 +59,22 @@ function create(){
     player.body.bounce.y = 0.2;
     player.body.gravity.y = 600;
 
+    //This create a randomized integer
+    var bottomWallSize = (Math.random() * (1.200 - 0.0200) + 0.0200).toFixed(4);
+    var topWallSize = 1.3 - bottomWallSize;
+    var topWallLocation = 600 - topWallSize;
+
     //This creates the first obstacle on the right side of the screen.
     obstacle = game.add.sprite(700, GAME_HEIGHT, 'obstacle');
-    obstacle.scale.setTo(1,1);
+    obstacle2 = game.add.sprite(700, 0, 'obstacle');
+    obstacle.scale.setTo(1,bottomWallSize);
+    obstacle2.scale.setTo(1,topWallSize);
     obstacle.anchor.setTo(0,1);
+    obstacle2.anchor.setTo(0,0);
     game.physics.arcade.enable(obstacle);
+    game.physics.arcade.enable(obstacle2);
     obstacle.body.immovable = true;
+    obstacle2.body.immovable = true;
 
     //This adds the scoreboard on the top left side of the screen.
     scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
@@ -74,18 +85,29 @@ function update(){
     //This is where the game engine recognizes collision betwen the player and the ground or the obstacle.
     game.physics.arcade.collide(player, ground);
     game.physics.arcade.collide(player, obstacle);
+    game.physics.arcade.collide(player, obstacle2);
 
     // //This will set the horizontal movement to zero.
     // player.body.velocity.x = 0;
+
+    //This create a randomized integer
+    var bottomWallSize = (Math.random() * (1.200 - 0.0200) + 0.0200).toFixed(4);
+    var topWallSize = 1.3 - bottomWallSize;
+    var topWallLocation = 600 - topWallSize;
 
     //This will create a new wall if the old wall goes off the screen.
     if (obstacle.x < 0) {
       obstacle.kill();
       obstacle = game.add.sprite(900, GAME_HEIGHT, 'obstacle');
-      obstacle.scale.setTo(1,1);
+      obstacle2 = game.add.sprite(900, 0, 'obstacle');
+      obstacle.scale.setTo(1,bottomWallSize);
+      obstacle2.scale.setTo(1,topWallSize);
       obstacle.anchor.setTo(0,1);
+      obstacle2.anchor.setTo(0,0);
       game.physics.arcade.enable(obstacle);
+      game.physics.arcade.enable(obstacle2);
       obstacle.body.immovable = true;
+      obstacle2.body.immovable = true;
     };
 
     // //This creates a place to add sound when the obstacle reaches the player.
@@ -97,6 +119,10 @@ function update(){
     //This will move the obstacle to the left if it is on the right side of the screen.
     if (obstacle.x > 600) {
       obstacle.x -= 0.05;
+    };
+
+    if (obstacle2.x > 600) {
+      obstacle2.x -= 0.05;
     };
 
     //This allows the player to jump only if you press the space key and the player is touching the something at the bottom.
@@ -115,6 +141,7 @@ function update(){
     if (player.x < 0){
       scoreText = game.add.text(350,200, 'You Lose!', {fill: '#ff0000'});
       obstacle.kill();
+      obstacle2.kill();
       player.kill();
     };
 };
